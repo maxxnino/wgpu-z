b3601168cf255b66732fa2838b5d0f1026f27515
maxxnino <34153891+maxxnino@users.noreply.github.com>
2021-12-18 21:58:51 +0900

    first triangle

--- src/main.zig
+++ src/main.zig
@@ -79,18 +79,117 @@ pub fn main() anyerror!void {
         .requiredFeaturesCount = 0,
         .requiredFeatures = undefined,
     }, requestDeviceCallback, @ptrCast(*c_void, &device));
-    const wgsl_descriptor = wb.WGPUShaderModuleWGSLDescriptor{
-        .chain = .{
-            .next = null,
-            .sType = .ShaderModuleWGSLDescriptor,
-        },
-        .source = triangle_shader,
-    };
+
     const shader = wgpu.wgpuDeviceCreateShaderModule(device, &.{
-        .nextInChain = wb.toChainedStruct(&wgsl_descriptor),
+        .nextInChain = wb.toChainedStruct(&wb.WGPUShaderModuleWGSLDescriptor{
+            .chain = .{
+                .next = null,
+                .sType = .ShaderModuleWGSLDescriptor,
+            },
+            .source = triangle_shader,
+        }),
         .label = null,
     });
+    const piptline_layout = wgpu.wgpuDeviceCreatePipelineLayout(device, &.{
+        .bindGroupLayoutCount = 0,
+        .bindGroupLayouts = null,
+    });
+    const swap_chain_format = wgpu.wgpuSurfaceGetPreferredFormat(surface, adapter);
+
+    const pipeline = wgpu.wgpuDeviceCreateRenderPipeline(device, &(wb.WGPURenderPipelineDescriptor){
+        .label = "Render pipeline",
+        .layout = piptline_layout,
+        .vertex = .{
+            .module = shader,
+            .entryPoint = "vs_main",
+            .constantCount = 0,
+            .constants = undefined,
+            .bufferCount = 0,
+            .buffers = undefined,
+        },
+        .primitive = .{
+            .topology = .TriangleList,
+            .stripIndexFormat = .Undefined,
+            .frontFace = .CCW,
+            .cullMode = .None,
+        },
+        .multisample = .{
+            .count = 1,
+            .mask = ~@as(u32, 0),
+            .alphaToCoverageEnabled = false,
+        },
+        .fragment = &.{
+            .module = shader,
+            .entryPoint = "fs_main",
+            .targetCount = 1,
+            .targets = @ptrCast([*]wb.WGPUColorTargetState, &wb.WGPUColorTargetState{
+                .format = swap_chain_format,
+                .blend = &.{
+                    .color = .{ .srcFactor = .One, .dstFactor = .Zero, .operation = .Add },
+                    .alpha = .{ .srcFactor = .One, .dstFactor = .Zero, .operation = .Add },
+                },
+                .writeMask = wb.WGPUColorWriteMask.all,
+            }),
+            .constantCount = 0,
+            .constants = undefined,
+        },
+        .depthStencil = null,
+    });
+
+    var prev_size = try window.getSize();
+    var swap_chain = wgpu.wgpuDeviceCreateSwapChain(device, surface, &.{
+        .usage = .{ .RenderAttachment = true },
+        .format = swap_chain_format,
+        .width = prev_size.width,
+        .height = prev_size.height,
+        .presentMode = .Fifo,
+    });
+
     while (!window.shouldClose()) {
+        const size = try window.getSize();
+        if (size.width != prev_size.width or size.height != prev_size.height) {
+            prev_size = size;
+
+            swap_chain = wgpu.wgpuDeviceCreateSwapChain(device, surface, &.{
+                .usage = .{ .RenderAttachment = true },
+                .format = swap_chain_format,
+                .width = size.width,
+                .height = size.height,
+                .presentMode = .Fifo,
+            });
+        }
+
+        const next_texture = wgpu.wgpuSwapChainGetCurrentTextureView(swap_chain);
+        std.debug.assert(next_texture != .null_handle);
+
+        const encoder = wgpu.wgpuDeviceCreateCommandEncoder(device, &.{ .label = "Command Encoder" });
+
+        const render_pass = wgpu.wgpuCommandEncoderBeginRenderPass(encoder, &.{
+            .colorAttachments = @ptrCast([*]const wb.WGPURenderPassColorAttachment, &wb.WGPURenderPassColorAttachment{
+                .view = next_texture,
+                .resolveTarget = .null_handle,
+                .loadOp = .Clear,
+                .storeOp = .Store,
+                .clearColor = .{
+                    .r = 0.0,
+                    .g = 1.0,
+                    .b = 0.0,
+                    .a = 1.0,
+                },
+            }),
+            .colorAttachmentCount = 1,
+            .depthStencilAttachment = null,
+            .occlusionQuerySet = undefined,
+        });
+        wgpu.wgpuRenderPassEncoderSetPipeline(render_pass, pipeline);
+        wgpu.wgpuRenderPassEncoderDraw(render_pass, 3, 1, 0, 0);
+        wgpu.wgpuRenderPassEncoderEndPass(render_pass);
+
+        const queue = wgpu.wgpuDeviceGetQueue(device);
+        const cmd_buffer = wgpu.wgpuCommandEncoderFinish(encoder, &.{ .label = null });
+        wgpu.wgpuQueueSubmit(queue, 1, @ptrCast([*]const wb.WGPUCommandBuffer, &cmd_buffer));
+        wgpu.wgpuSwapChainPresent(swap_chain);
+
         try glfw.pollEvents();
     }
 }
